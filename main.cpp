9d53fd835ea9ea729b99c8576b0532413779d0a5
mmtq917 <35444587+mmtq917@users.noreply.github.com>
2018-11-24 14:31:52 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -29,20 +29,32 @@ int printGrid(int matrix){
         }
         cout << endl;
     }*/
-  char candies[] = {'c','O','i','N','d','p','z','x','T','@','#','$','&','*'}; // These are all the candies we will use. Special characters are special candies and vice versa
   
   
   
   return fuckUpStatus;                      // fuckUpStatus tells if everything goes right or not! 
 }
 
+int fillCandies(int level){
+  char totalCandies[] = {'c','O','i','N','d','p','z','x','T','@','#','$','&','*'}; // These are all the candies we will use. Special characters are special candies and vice versa
+  char candies[level*3];                    // Can dies used in the current level
+  for(int i=0; i<= level*3 ; i++){          // Selecting candies to use in the curent level.
+    candies[i] = totalCandies[i];
+  }
+  for(int i=0; i<= level*3 ; i++){
+    cout << candies[i] ;
+  }
+  return 0;
+}
+
 int candyCrush(int level){                  // The fucking game, Takes input the level. level range from (1-3)
   printGrid(level * 5) ;                    // print the grid for the condies  
   Sleep(2000);
+  fillCandies(level);
 }
 
 int main(){
-  salamDua();
+  salamDua();                              // Every fucking traditional thing, like Hello, welcome to this f**. Give a f**, and get lost etc. 
   switch(game()){
     case 1:                                // If user selects to continue
       candyCrush(selectLevel());           // Ask for selecting Level, and then put it as argument for starting the game.
